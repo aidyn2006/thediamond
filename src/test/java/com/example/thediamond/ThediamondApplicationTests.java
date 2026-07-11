fccf521b251973bf4d715dec6045990bfbf1cdbc
a.nurlan <a.nurlan@documentolog.com>
2026-07-11 12:41:02 +0500

    Initial commit

---
+++ src/test/java/com/example/thediamond/ThediamondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thediamond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThediamondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
